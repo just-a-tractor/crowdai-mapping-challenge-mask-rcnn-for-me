c01f556bad9b08d172239ecdff266afeb46d3e31
just-a-tractor <45266305+just-a-tractor@users.noreply.github.com>
2022-09-02 19:25:23 +0300

    Update coco.py

--- samples/coco/coco.py
+++ samples/coco/coco.py
@@ -78,13 +78,13 @@ class CocoConfig(Config):
 
     # We use a GPU with 12GB memory, which can fit two images.
     # Adjust down if you use a smaller GPU.
-    IMAGES_PER_GPU = 2
+    IMAGES_PER_GPU = 8
 
     # Uncomment to train on 8 GPUs (default is 1)
     # GPU_COUNT = 8
 
     # Number of classes (including background)
-    NUM_CLASSES = 1 + 80  # COCO has 80 classes
+    NUM_CLASSES = 1 + 1  # COCO has 80 classes
 
 
 ############################################################
